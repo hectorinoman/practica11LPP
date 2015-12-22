6ea4f5699f37e992475bedcee64911405ec2600c
hectorinoman <hectorinoman@gmail.com>
2015-12-22 15:26:47 +0000

    declaraciones

--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -2,5 +2,13 @@ require 'spec_helper'
 require 'referencia'
 
 describe Referencia do
-
+     before :all do
+        @a = Book.new('The Rspec book 2010') do
+            autor 'Charles Lip'
+            fecha 'April 29, 2014'
+            serie 'ninguna'
+            editorial 'Lisin'
+            edicion '2010 edition'
+        end
+     end
 end
